e8f4245ccdb2dae2454725a2c7813f6a92e38115
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-10 17:10:56 -0500

    chore(git): added git hooks

--- bin/as-a.js
+++ bin/as-a.js
@@ -3,13 +3,16 @@
 const debug = require('debug')('as-a')
 const la = require('lazy-ass')
 const is = require('check-more-types')
+const join = require('path').join
 
 require('simple-bin-help')({
   minArguments: 3,
-  packagePath: __dirname + '/../package.json',
+  packagePath: join(__dirname, '..', 'package.json'),
   help: 'use: as-a <env settings name> [command with options]'
-});
+})
 
 const name = process.argv[2]
 const command = process.argv.slice(3)
-console.log(name, '-', command)
+debug(name, '-', command)
+
+la(is.unemptyString(name), 'expected env name', process.argv)
